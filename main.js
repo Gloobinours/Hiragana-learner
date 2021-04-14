f25ce30257ea3043a9ddadff213ab1495669fcdb
Gloobinours <kusami.louan@gmail.com>
2021-04-14 08:58:55 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,42 @@
+
+// Returns the random number generated + the hiragana
+var randomLatin = () => {
+    let keys = []
+    if (simpleHiraganaCheckbox.checked) {
+        // gets array of keys in object hiraganas
+        // keys = Object.keys( hiraganas );
+        keys = keys.concat(Object.keys( hiraganas ));
+    }
+    if (doubleHiraganaCheckbox.checked) {
+        // gets array of keys in object hiraganas
+        // keys = Object.keys( hiraganasDouble );
+        keys = keys.concat(Object.keys( hiraganasDouble ));
+    }
+    // randomly takes a key from the object hiraganas
+    return keys[ Math.floor( keys.length * Math.random() ) ];
+};
+
+const hiraganaOutput = document.getElementById('hiragana');
+const input = document.getElementById('input');
+const answerElement = document.getElementById('answer');
+const simpleHiraganaCheckbox = document.getElementById('simpleHiraganaCheckbox');
+const doubleHiraganaCheckbox = document.getElementById('doubleHiraganaCheckbox');
+
+let answer = randomLatin(); // Stores the value returned by randomLatin
+hiraganaOutput.innerHTML = hiraganas[answer] || hiraganasDouble[answer]; // Displays the hiraganas on the screen
+
+// Listens for the 'enter' key
+input.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        // if the input value is equal to the answer: executes randomLation(), changes the top modal to display:none
+        if (answer === input.value) {
+            answer = randomLatin();
+            hiraganaOutput.innerHTML = hiraganas[answer] || hiraganasDouble[answer];
+            answerElement.style.display = 'none';
+        } else { // if the input value is different from the answer: changes the top modal to display:flex, pushes the answer in modal
+            answerElement.style.display = 'flex';
+            answerElement.innerHTML = answer;
+        }
+        input.value = ''; // resets the input value to nothing
+    }
+})
